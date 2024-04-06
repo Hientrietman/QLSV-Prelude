665f239f8d4a15890ebaa893f9dc96c91f635301
Sanglaga18 <Minhhoang0121@gmail.com>
2024-04-06 15:11:11 +0700

    06042024-PQLSV-41-Hoàng-Thêm giao diện và chức năng xem danh sách sinh viên của lớp học phần

---
+++ DTO/HocPhanDTO.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class HocPhanDTO
+    {
+
+        public HocPhanDTO(string maHocPhan, string maMonHoc, string tenHocPhan, string maGiaoVien, string nam, string phongHoc)
+        {
+            this.MaHP = maHocPhan;
+            this.MaMH = maMonHoc;
+            this.TenHocPhan = tenHocPhan;
+            this.Nam = nam;
+            this.MaGV = maGiaoVien;
+            this.PhongHoc = phongHoc;
+        }
+
+        public HocPhanDTO(DataRow row)
+        {
+            this.MaHP = row["maHocPhan"].ToString();
+            this.MaMH = row["maMonHoc"].ToString();
+            this.TenHocPhan = row["tenHocPhan"].ToString();
+            this.Nam = row["nam"].ToString();
+            this.MaGV = row["MaGV"].ToString();
+            this.PhongHoc = row["phongHoc"].ToString();
+        }
+
+        private string maHP;
+
+        public string MaHP
+        {
+            get { return maHP; }
+            set { maHP = value; }
+        }
+
+        private string maMH;
+
+        public string MaMH
+        {
+            get { return maMH; }
+            set { maMH = value; }
+        }
+
+        private string tenHocPhan;
+
+        public string TenHocPhan
+        {
+            get { return tenHocPhan; }
+            set { tenHocPhan = value; }
+        }
+
+        private string maGV;
+
+        public string MaGV
+        {
+            get { return maGV; }
+            set { maGV = value; }
+        }
+
+        private string nam;
+
+        public string Nam
+        {
+            get { return nam; }
+            set { nam = value; }
+        }
+
+        private string phongHoc;
+
+        public string PhongHoc
+        {
+            get { return phongHoc; }
+            set { phongHoc = value; }
+        }
+    }
+}
---
+++ GUI/DanhSachSinhVienCuaLopHP.cs
@@ -0,0 +1,77 @@
+﻿using BUS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace GUI
+{
+    public partial class DanhSachSinhVienCuaLopHP : Form
+    {
+        public DanhSachSinhVienCuaLopHP()
+        {
+            InitializeComponent();
+            dtgvDanhSachSVCuaLopHocPhan.DataSource = SinhVienBUS.Instance.LayDanhSachSVCuaLopHocPhan(lblTenLopHocPhan.Text);
+        }
+
+        DataTable TimKiemSVTrongLopHocPhan(string MaHP, string MaSV)
+        {
+            DataTable dataTable = new DataTable();
+
+            dataTable = SinhVienBUS.Instance.TimKiemMaSVTrongLopHocPhan(MaHP, MaSV);
+
+            return dataTable;
+        }
+
+        private void btnTimMaSinhVien_Click(object sender, EventArgs e)
+        {
+            dtgvDanhSachSVCuaLopHocPhan.DataSource = TimKiemSVTrongLopHocPhan(lblTenLopHocPhan.Text, txtTimMaSinhVien.Text);
+        }
+
+        private void XuatExcel(DataGridView dataGridView)
+        {
+            try
+            {
+                Excel.Application excelApp = new Excel.Application();
+                excelApp.Visible = true;
+
+                Excel.Workbook workbook = excelApp.Workbooks.Add();
+
+                Excel.Worksheet worksheet = (Excel.Worksheet)workbook.Worksheets[1];
+
+                for (int i = 2; i < dataGridView.Columns.Count; i++)
+                {
+                    worksheet.Cells[1, i - 1] = dataGridView.Columns[i].HeaderText;
+                }
+
+                for (int i = 0; i < dataGridView.Rows.Count; i++)
+                {
+                    for (int j = 2; j < dataGridView.Columns.Count; j++)
+                    {
+                        worksheet.Cells[i + 2, j - 1] = dataGridView.Rows[i].Cells[j].Value;
+                    }
+                }
+
+                worksheet.Columns.AutoFit();
+
+                //workbook.Activate();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message, "Thông báo lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnXuatExcel_Click(object sender, EventArgs e)
+        {
+            XuatExcel(dtgvDanhSachSVCuaLopHocPhan);
+        }
+    }
+}
