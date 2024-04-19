36aab491781adc5055211ca2f07f9842c2bd61e0
Do Quoc Huy <doquochuy19102019@gmail.com>
2024-04-19 19:50:28 +0700

    19042024-PQLSV-113-Huy-Xử lí chức năng thêm lớp học phần vào tkb của sv

--- BUS/HocPhanBUS.cs
+++ BUS/HocPhanBUS.cs
@@ -22,10 +22,38 @@ namespace BUS
         {
             return HocPhanDAO.Instance.TimKiemMaHP(MaHP);
         }
+        public DataTable TimKiemLopHP(int NamHoc, string MaHP)
+        {
+            return HocPhanDAO.Instance.TimKiemLopHP(NamHoc,MaHP);
+        }
+        public bool KiemTraNamHoc(int kiemtraNamHoc, string MaHocPhan)
+        {
+            return HocPhanDAO.Instance.KiemTraNamHoc(kiemtraNamHoc, MaHocPhan);
+        }
+        public bool KiemTraHocPhan(string MSSV, int HocKy, string NamHoc, string MaHP)
+        {
+            return HocPhanDAO.Instance.KiemTraHocPhan(MSSV, HocKy, NamHoc, MaHP);
+        }
+        public bool KiemTraMonHoc(string MSSV, int HocKy, string NamHoc, string MaHP, string MaMH)
+        {
+            return HocPhanDAO.Instance.KiemTraMonHoc(MSSV, HocKy, NamHoc, MaHP, MaMH);
+        }
 
         public DataTable LayDanhSachHocPhan()
         {
             return HocPhanDAO.Instance.LayDanhSachHocPhan();
+        }
+        public DataTable LayDanhSachHocPhanCuaSV(string MSSV, int HocKy, string NamHoc)
+        {
+            return HocPhanDAO.Instance.LayDanhSachHocPhanCuaSV(MSSV,HocKy,NamHoc);
+        }
+        public DataTable ThemHocPhan(string MSSV, int HocKy, string NamHoc, string MaHP)
+        {
+            return HocPhanDAO.Instance.ThemHocPhan(MSSV, HocKy, NamHoc,MaHP);
+        }
+        public DataTable TimThoiKhoaBieu(string MSSV, int HocKy, string NamHoc)
+        {
+            return HocPhanDAO.Instance.TimThoiKhoaBieu(MSSV, HocKy, NamHoc);
         }
     }
 }
---
+++ GUI/InThoiKhoaBieu.cs
@@ -0,0 +1,50 @@
+﻿using BUS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class InThoiKhoaBieu : Form
+    {
+        public InThoiKhoaBieu()
+        {
+            InitializeComponent();
+            loadThoiKhoaBieu();
+        }
+        private void loadThoiKhoaBieu()
+        {
+            string HoTenSV = lblTenSV.Text;
+            string MSSV = lblMSSV.Text;
+            string Lop = lblLop.Text;
+            string SoDienThoai = lblSDT.Text;
+
+            int HocKy = int.Parse(lblHocKy.Text);
+            string NamHocSV = lblNamHoc.Text;
+
+            dtgvBangTKB.DataSource = HocPhanBUS.Instance.TimThoiKhoaBieu(MSSV, HocKy, NamHocSV);
+
+            ////Xử lý tách chuỗi năm học
+            //string[] arrNamHocSV = NamHocSV.Split(new char[] { '-' });
+
+            //int kiemtraNamHoc;
+            //if (HocKy == 1)
+            //{
+            //    kiemtraNamHoc = int.Parse(arrNamHocSV[0]);
+            //    dtgvBangTKB.DataSource = HocPhanBUS.Instance.TimThoiKhoaBieu(MSSV, HocKy, NamHocSV);
+            //}
+            //else if (HocKy == 2)
+            //{
+            //    kiemtraNamHoc = int.Parse(arrNamHocSV[1]);
+            //    dtgvBangTKB.DataSource = HocPhanBUS.Instance.TimThoiKhoaBieu(MSSV, HocKy, NamHocSV);
+            //}
+        }
+
+    }
+}
