812ec0d085e05944721bd097bcb4d4d0917f83ce
B-baoha-H <baoha2312@gmail.com>
2024-05-13 16:36:01 +0700

    13052024-PQLSV-121-Hà-Lỗi khi in thời khóa biểu mà không chọn học kỳ và năm học

--- GUI/SinhVienXemThoiKhoaBieu.Designer.cs
+++ GUI/SinhVienXemThoiKhoaBieu.Designer.cs
@@ -70,6 +70,7 @@
             this.cboNamHoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cboNamHoc.FormattingEnabled = true;
             this.cboNamHoc.Items.AddRange(new object[] {
+            "2020-2021",
             "2021-2022",
             "2022-2023",
             "2023-2024"});
